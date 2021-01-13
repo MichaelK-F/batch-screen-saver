87bda6fc211034adb258d35c7752d59e0a9e0b40
Michael K-F <63935978+MichaelK-F@users.noreply.github.com>
2021-01-13 12:22:31 -0500

    Add files via upload

---
+++ screen saver.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:VBSDynamicBuild
+SET TempVBSFile=%temp%\~tmpSendKeysTemp.vbs
+IF EXIST "%TempVBSFile%" DEL /F /Q "%TempVBSFile%"
+ECHO Set WshShell = WScript.CreateObject("WScript.Shell") >>"%TempVBSFile%"
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+ECHO WshShell.SendKeys "{F11}"                            >>"%TempVBSFile%
+ECHO Wscript.Sleep 900                                    >>"%TempVBSFile%"
+CSCRIPT //nologo "%TempVBSFile%"
+
+
+pause>>NUL
